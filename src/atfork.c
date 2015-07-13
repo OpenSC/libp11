342d0b8645a375777947de62e0026bc9cf13ce30
Nikos Mavrogiannopoulos <nmav@redhat.com>
2015-07-13 13:24:57 +0200

    removed references to gnutls

--- src/atfork.c
+++ src/atfork.c
@@ -4,9 +4,7 @@
  *
  * Author: Nikos Mavrogiannopoulos
  *
- * This file is part of GnuTLS.
- *
- * The GnuTLS is free software; you can redistribute it and/or
+ * This is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 2.1 of
  * the License, or (at your option) any later version.
