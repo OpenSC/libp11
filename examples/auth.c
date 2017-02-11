463a9f1a0d6a5257dac42a7563faf65ad23de4eb
Bernard Spil <brnrd@FreeBSD.org>
2017-02-11 20:39:09 +0100

    Fix build issues with LibreSSL
      - LibreSSL defines OPENSSL_VERSION_NUMBER as 0x20000000L
      - Additionally defines LIBRESSL_VERSION_NUMBER
      - Add additional checks for OpenSSL 1.1.0-only features
    
    Signed-off-by: Bernard Spil <brnrd@FreeBSD.org>

--- examples/auth.c
+++ examples/auth.c
@@ -212,7 +212,7 @@ int main(int argc, char *argv[])
 
 	/* now verify the result */
 	rc = RSA_verify(NID_sha1, random, RANDOM_SIZE,
-#if OPENSSL_VERSION_NUMBER >= 0x10100003L
+#if OPENSSL_VERSION_NUMBER >= 0x10100003L && !defined(LIBRESSL_VERSION_NUMBER)
 			signature, siglen, EVP_PKEY_get0_RSA(pubkey));
 #else
 			signature, siglen, pubkey->pkey.rsa);
--- src/libp11.h
+++ src/libp11.h
@@ -370,7 +370,7 @@ extern int PKCS11_generate_random(PKCS11_SLOT *slot, unsigned char *r, unsigned 
  */
 RSA_METHOD *PKCS11_get_rsa_method(void);
 /* Also define unsupported methods to retain backward compatibility */
-#if OPENSSL_VERSION_NUMBER >= 0x10100002L
+#if OPENSSL_VERSION_NUMBER >= 0x10100002L && !defined(LIBRESSL_VERSION_NUMBER)
 EC_KEY_METHOD *PKCS11_get_ec_key_method(void);
 void *PKCS11_get_ecdsa_method(void);
 void *PKCS11_get_ecdh_method(void);
