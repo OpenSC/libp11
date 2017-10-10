472a038c12c6f3ab8946f63c01edd81ddbc9a3ed
Michał Trojnara <Michal.Trojnara@stunnel.org>
2017-10-10 22:05:09 +0200

    OpenSSL 1.1.0 support for #181

--- src/eng_front.c
+++ src/eng_front.c
@@ -191,24 +191,38 @@ static EVP_PKEY *load_pubkey(ENGINE *engine, const char *s_key_id,
 	return ctx_load_pubkey(ctx, s_key_id, ui_method, callback_data);
 }
 
+#ifndef EVP_F_EVP_PKEY_SET1_ENGINE
+
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+struct evp_pkey_st {
+	int type;
+	int save_type;
+	int references;
+	const EVP_PKEY_ASN1_METHOD *ameth;
+	ENGINE *engine;
+	/* we don't need the remaining fields */
+};
+#endif /* OPENSSL_VERSION_NUMBER */
+
+static void EVP_PKEY_set1_engine(EVP_PKEY *pkey, ENGINE *engine)
+{
+	pkey->engine = engine;
+}
+
+#endif /* EVP_F_EVP_PKEY_SET1_ENGINE */
+
 static EVP_PKEY *load_privkey(ENGINE *engine, const char *s_key_id,
 		UI_METHOD *ui_method, void *callback_data)
 {
-	ENGINE_CTX *ctx  = NULL;
-	EVP_PKEY   *pkey = NULL;
+	ENGINE_CTX *ctx;
+	EVP_PKEY *pkey;
 
 	ctx = get_ctx(engine);
 	if (ctx == NULL)
 		return 0;
 	pkey = ctx_load_privkey(ctx, s_key_id, ui_method, callback_data);
 	if (pkey)
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
-		pkey->engine = engine;
-#else
-#if defined(EVP_F_EVP_PKEY_SET1_ENGINE)
-		EVP_PKEY_set1_engine(pkey,engine);
-#endif /* EVP_F_EVP_PKEY_SET1_ENGINE */
-#endif /* OPENSSL_VERSION_NUMBER */
+		EVP_PKEY_set1_engine(pkey, engine);
 	return pkey;
 }
 
