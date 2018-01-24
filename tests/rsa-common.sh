d28bf8d1ba43bdccfbb2927110685ca6a9d51fc3
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2018-01-24 18:48:42 +0100

    Improve tests common script

--- tests/rsa-common.sh
+++ tests/rsa-common.sh
@@ -10,7 +10,7 @@
 #
 # GnuTLS is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
 # General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
@@ -23,13 +23,14 @@ echo "Output directory: ${outdir}"
 
 mkdir -p $outdir
 
-for i in /usr/lib64/pkcs11 /usr/lib/softhsm /usr/local/lib/softhsm /opt/local/lib/softhsm /usr/lib/x86_64-linux-gnu/softhsm /usr/lib /usr/lib64/softhsm;do
+# Set the module to be used
+for i in /usr/lib64/pkcs11 /usr/lib64/softhsm /usr/lib/x86_64-linux-gnu/softhsm /usr/local/lib/softhsm /opt/local/lib/softhsm /usr/lib/softhsm /usr/lib ;do
 	if test -f "$i/libsofthsm2.so"; then
-		ADDITIONAL_PARAM="$i/libsofthsm2.so"
+		MODULE="$i/libsofthsm2.so"
 		break
 	else
 		if test -f "$i/libsofthsm.so";then
-			ADDITIONAL_PARAM="$i/libsofthsm.so"
+			MODULE="$i/libsofthsm.so"
 			break
 		fi
 	fi
@@ -39,28 +40,30 @@ if (! test -x /usr/bin/pkcs11-tool && ! test -x /usr/local/bin/pkcs11-tool);then
 	exit 77
 fi
 
-init_card () {
-	PIN="$1"
-	PUK="$2"
+# Initialize the SoftHSM DB
+init_db () {
+	if test -x "/usr/bin/softhsm"; then
+		export SOFTHSM_CONF="$outdir/softhsm-testpkcs11.config"
+		SOFTHSM_TOOL="/usr/bin/softhsm"
+		SLOT="--slot 0"
+	fi
 
 	if test -x "/usr/local/bin/softhsm2-util"; then
 		export SOFTHSM2_CONF="$outdir/softhsm-testpkcs11.config"
 		SOFTHSM_TOOL="/usr/local/bin/softhsm2-util"
+		SLOT="--free "
 	fi
 
 	if test -x "/opt/local/bin/softhsm2-util"; then
 		export SOFTHSM2_CONF="$outdir/softhsm-testpkcs11.config"
 		SOFTHSM_TOOL="/opt/local/bin/softhsm2-util"
+		SLOT="--free "
 	fi
 
 	if test -x "/usr/bin/softhsm2-util"; then
 		export SOFTHSM2_CONF="$outdir/softhsm-testpkcs11.config"
 		SOFTHSM_TOOL="/usr/bin/softhsm2-util"
-	fi
-
-	if test -x "/usr/bin/softhsm"; then
-		export SOFTHSM_CONF="$outdir/softhsm-testpkcs11.config"
-		SOFTHSM_TOOL="/usr/bin/softhsm"
+		SLOT="--free "
 	fi
 
 	if test -z "${SOFTHSM_TOOL}"; then
@@ -68,19 +71,27 @@ init_card () {
 		exit 77
 	fi
 
-	if test -z "${SOFTHSM_CONF}"; then
+	if test -n "${SOFTHSM2_CONF}"; then
 		rm -rf $outdir/softhsm-testpkcs11.db
 		mkdir -p $outdir/softhsm-testpkcs11.db
 		echo "objectstore.backend = file" > "${SOFTHSM2_CONF}"
-		echo "directories.tokendir = $outdir/softhsm-testpkcs11.db" >> "${SOFTHSM2_CONF}"
+		echo "directories.tokendir = $outdir/softhsm-testpkcs11.db" >> \
+			"${SOFTHSM2_CONF}"
 	else
 		rm -rf $outdir/softhsm-testpkcs11.db
 		echo "0:$outdir/softhsm-testpkcs11.db" > "${SOFTHSM_CONF}"
 	fi
+}
 
+# Create a new device
+init_card () {
+	PIN="$1"
+	PUK="$2"
+	DEV_LABEL="$3"
 
 	echo -n "* Initializing smart card... "
-	${SOFTHSM_TOOL} --init-token --slot 0 --label "libp11-test" --so-pin "${PUK}" --pin "${PIN}" >/dev/null
+	${SOFTHSM_TOOL} --init-token ${SLOT} --label "${DEV_LABEL}" \
+		--so-pin "${PUK}" --pin "${PIN}" >/dev/null
 	if test $? = 0; then
 		echo ok
 	else
@@ -89,27 +100,52 @@ init_card () {
 	fi
 }
 
-PIN=1234
-PUK=1234
-init_card $PIN $PUK
+# Import objects to the token
+import_objects () {
+	ID=$1
+	OBJ_LABEL=$2
 
-# generate key in token
-pkcs11-tool -p $PIN --module $ADDITIONAL_PARAM -d 01020304 -a server-key -l -w ${srcdir}/rsa-prvkey.der -y privkey >/dev/null
-if test $? != 0;then
-	exit 1;
-fi
+	pkcs11-tool -p ${PIN} --module ${MODULE} -d ${ID} -a ${OBJ_LABEL} -l -w ${srcdir}/rsa-prvkey.der -y privkey >/dev/null
+		if test $? != 0;then
+			exit 1;
+	fi
 
-pkcs11-tool -p $PIN --module $ADDITIONAL_PARAM -d 01020304 -a server-key -l -w ${srcdir}/rsa-pubkey.der -y pubkey >/dev/null
-if test $? != 0;then
-	exit 1;
-fi
+		pkcs11-tool -p ${PIN} --module ${MODULE} -d ${ID} -a ${OBJ_LABEL} -l -w ${srcdir}/rsa-pubkey.der -y pubkey >/dev/null
+		if test $? != 0;then
+			exit 1;
+	fi
 
-pkcs11-tool -p $PIN --module $ADDITIONAL_PARAM -d 01020304 -a server-key -l -w ${srcdir}/rsa-cert.der -y cert >/dev/null
-if test $? != 0;then
-	exit 1;
-fi
+		pkcs11-tool -p ${PIN} --module ${MODULE} -d ${ID} -a ${OBJ_LABEL} -l -w ${srcdir}/rsa-cert.der -y cert >/dev/null
+		if test $? != 0;then
+			exit 1;
+	fi
 
-echo "***************"
-echo "Listing objects"
-echo "***************"
-pkcs11-tool -p $PIN --module $ADDITIONAL_PARAM -l -O
+	echo Finished
+}
+
+# List the objects contained in the token
+list_objects () {
+	echo "***************"
+	echo "Listing objects"
+	echo "***************"
+	pkcs11-tool -p ${PIN} --module ${MODULE} -l -O
+}
+
+common_init () {
+	# Set the used PIN and PUK
+	PIN=1234
+	PUK=1234
+
+	# Initialize the SoftHSM DB
+	init_db
+
+	# Initialize a new device
+	init_card $PIN $PUK "libp11-test"
+
+	echo Importing
+	# Import the used objects (private key, public key, and certificate)
+	import_objects 01020304 "server-key"
+
+	# List the imported objects
+	list_objects
+}
