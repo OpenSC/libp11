OpenSC/libp11

commit 14cd0d328fff96b79fabcc30257e358399c8ad25

Fixed a slot list memory leak in the engine code

src/eng_back.c
@@ -36,6 +36,8 @@
 
 struct st_engine_ctx {
 	PKCS11_CTX *pkcs11_ctx;
+	PKCS11_SLOT *slot_list;
+	unsigned int slot_count;
 	/*
 	 * The PIN used for login. Cache for the get_pin function.
 	 * The memory for this PIN is always owned internally,
@@ -123,6 +125,10 @@ ENGINE_CTX *pkcs11_new()
 int pkcs11_finish(ENGINE_CTX *ctx)
 {
 	if (ctx) {
+		if (ctx->slot_list) {
+			PKCS11_release_all_slots(ctx->pkcs11_ctx,
+				ctx->slot_list, ctx->slot_count);
+		}
 		if (ctx->pkcs11_ctx) {
 			PKCS11_CTX_unload(ctx->pkcs11_ctx);
 			PKCS11_CTX_free(ctx->pkcs11_ctx);
@@ -154,6 +160,15 @@ int pkcs11_init(ENGINE_CTX *ctx)
 		fprintf(stderr, "Unable to load module %s\n", mod);
 		return 0;
 	}
+	if (PKCS11_enumerate_slots(ctx->pkcs11_ctx,
+			&ctx->slot_list, &ctx->slot_count) < 0) {
+		fprintf(stderr, "Failed to enumerate slots\n");
+		return 0;
+	}
+	if (ctx->verbose) {
+		fprintf(stderr, "Found %u slot%s\n", ctx->slot_count,
+			(ctx->slot_count <= 1) ? "" : "s");
+	}
 	return 1;
 }
 
@@ -166,12 +181,12 @@ int pkcs11_init(ENGINE_CTX *ctx)
 
 static X509 *pkcs11_load_cert(ENGINE_CTX *ctx, const char *s_slot_cert_id)
 {
-	PKCS11_SLOT *slot_list, *slot;
+	PKCS11_SLOT *slot;
 	PKCS11_SLOT *found_slot = NULL;
 	PKCS11_TOKEN *tok, *match_tok = NULL;
 	PKCS11_CERT *certs, *selected_cert = NULL;
 	X509 *x509;
-	unsigned int slot_count, cert_count, n, m;
+	unsigned int cert_count, n, m;
 	unsigned char cert_id[MAX_VALUE_LEN / 2];
 	size_t cert_id_len = sizeof(cert_id);
 	char *cert_label = NULL;
@@ -226,17 +241,8 @@ static X509 *pkcs11_load_cert(ENGINE_CTX *ctx, const char *s_slot_cert_id)
 		}
 	}
 
-	if (PKCS11_enumerate_slots(ctx->pkcs11_ctx, &slot_list, &slot_count) < 0) {
-		fprintf(stderr, "Failed to enumerate slots\n");
-		return NULL;
-	}
-
-	if (ctx->verbose) {
-		fprintf(stderr, "Found %u slot%s\n", slot_count,
-			(slot_count <= 1) ? "" : "s");
-	}
-	for (n = 0; n < slot_count; n++) {
-		slot = slot_list + n;
+	for (n = 0; n < ctx->slot_count; n++) {
+		slot = ctx->slot_list + n;
 		flags[0] = '\0';
 		if (slot->token) {
 			if (!slot->token->initialized)
@@ -295,20 +301,19 @@ static X509 *pkcs11_load_cert(ENGINE_CTX *ctx, const char *s_slot_cert_id)
 		fprintf(stderr, "Specified object not found\n");
 		return NULL;
 	} else if (slot_nr == -1) {
-		if (!(slot = PKCS11_find_token(ctx->pkcs11_ctx, slot_list, slot_count))) {
+		if (!(slot = PKCS11_find_token(ctx->pkcs11_ctx,
+				ctx->slot_list, ctx->slot_count))) {
 			fprintf(stderr, "No tokens found\n");
 			return NULL;
 		}
 	} else {
 		fprintf(stderr, "Invalid slot number: %d\n", slot_nr);
-		PKCS11_release_all_slots(ctx->pkcs11_ctx, slot_list, slot_count);
 		return NULL;
 	}
 	tok = slot->token;
 
 	if (tok == NULL) {
 		fprintf(stderr, "Empty token found\n");
-		PKCS11_release_all_slots(ctx->pkcs11_ctx, slot_list, slot_count);
 		return NULL;
 	}
 
@@ -330,7 +335,6 @@ static X509 *pkcs11_load_cert(ENGINE_CTX *ctx, const char *s_slot_cert_id)
 
 	if (PKCS11_enumerate_certs(tok, &certs, &cert_count)) {
 		fprintf(stderr, "Unable to enumerate certificates\n");
-		PKCS11_release_all_slots(ctx->pkcs11_ctx, slot_list, slot_count);
 		return NULL;
 	}
 
@@ -358,7 +362,6 @@ static X509 *pkcs11_load_cert(ENGINE_CTX *ctx, const char *s_slot_cert_id)
 
 	if (selected_cert == NULL) {
 		fprintf(stderr, "Certificate not found.\n");
-		PKCS11_release_all_slots(ctx->pkcs11_ctx, slot_list, slot_count);
 		return NULL;
 	}
 
@@ -451,13 +454,13 @@ static int pkcs11_login(ENGINE_CTX *ctx, PKCS11_SLOT *slot, PKCS11_TOKEN *tok,
 static EVP_PKEY *pkcs11_load_key(ENGINE_CTX *ctx, const char *s_slot_key_id,
 		UI_METHOD * ui_method, void *callback_data, int isPrivate)
 {
-	PKCS11_SLOT *slot_list, *slot;
+	PKCS11_SLOT *slot;
 	PKCS11_SLOT *found_slot = NULL;
 	PKCS11_TOKEN *tok, *match_tok = NULL;
 	PKCS11_KEY *keys, *selected_key = NULL;
 	PKCS11_CERT *certs;
 	EVP_PKEY *pk;
-	unsigned int slot_count, cert_count, key_count, n, m;
+	unsigned int cert_count, key_count, n, m;
 	unsigned char key_id[MAX_VALUE_LEN / 2];
 	size_t key_id_len = sizeof(key_id);
 	char *key_label = NULL;
@@ -517,17 +520,8 @@ static EVP_PKEY *pkcs11_load_key(ENGINE_CTX *ctx, const char *s_slot_key_id,
 		}
 	}
 
-	if (PKCS11_enumerate_slots(ctx->pkcs11_ctx, &slot_list, &slot_count) < 0) {
-		fprintf(stderr, "Failed to enumerate slots\n");
-		return NULL;
-	}
-
-	if (ctx->verbose) {
-		fprintf(stderr, "Found %u slot%s\n", slot_count,
-			(slot_count <= 1) ? "" : "s");
-	}
-	for (n = 0; n < slot_count; n++) {
-		slot = slot_list + n;
+	for (n = 0; n < ctx->slot_count; n++) {
+		slot = ctx->slot_list + n;
 		flags[0] = '\0';
 		if (slot->token) {
 			if (!slot->token->initialized)
@@ -586,20 +580,19 @@ static EVP_PKEY *pkcs11_load_key(ENGINE_CTX *ctx, const char *s_slot_key_id,
 		fprintf(stderr, "Specified object not found\n");
 		return NULL;
 	} else if (slot_nr == -1) {
-		if (!(slot = PKCS11_find_token(ctx->pkcs11_ctx, slot_list, slot_count))) {
+		if (!(slot = PKCS11_find_token(ctx->pkcs11_ctx,
+				ctx->slot_list, ctx->slot_count))) {
 			fprintf(stderr, "No tokens found\n");
 			return NULL;
 		}
 	} else {
 		fprintf(stderr, "Invalid slot number: %d\n", slot_nr);
-		PKCS11_release_all_slots(ctx->pkcs11_ctx, slot_list, slot_count);
 		return NULL;
 	}
 	tok = slot->token;
 
 	if (tok == NULL) {
 		fprintf(stderr, "Found empty token\n");
-		PKCS11_release_all_slots(ctx->pkcs11_ctx, slot_list, slot_count);
 		return NULL;
 	}
 	/* The following check is non-critical to ensure interoperability
@@ -608,7 +601,6 @@ static EVP_PKEY *pkcs11_load_key(ENGINE_CTX *ctx, const char *s_slot_key_id,
 		fprintf(stderr, "Found uninitialized token\n");
 	if (isPrivate && !tok->userPinSet && !tok->readOnly) {
 		fprintf(stderr, "Found slot without user PIN\n");
-		PKCS11_release_all_slots(ctx->pkcs11_ctx, slot_list, slot_count);
 		return NULL;
 	}
 

commit 7275c4b8f681846cc5d9acc4ae9e6b186f8267bf

add re-numeration of slots as engine ctrl command
This was broken in  14cd0d328fff96b79fabcc30257e358399c8ad25.
Previously, the engine would re-enumerate before loading keys/certs
Not re-enumerating the slots results in un-awareness of changes in slots
and tokens.
This awareness is required to be able to change the token in a slot at
runtime, else you use invalid sessions
(PKCS#11 module:pkcs11_find_keys:Session handle invalid:p11_key.c:512)

The patch adds the command RE_ENUMERATE as engine control, providing the
ability to re-enumerate on demand/when required.

src/eng_back.c
@@ -264,24 +264,17 @@ int ctx_destroy(ENGINE_CTX *ctx)
 	return 1;
 }
 
-/* Initialize libp11 data: ctx->pkcs11_ctx and ctx->slot_list */
-static void ctx_init_libp11_unlocked(ENGINE_CTX *ctx)
+static int ctx_enumerate_slots(ENGINE_CTX *ctx, PKCS11_CTX *pkcs11_ctx)
 {
-	PKCS11_CTX *pkcs11_ctx;
 	PKCS11_SLOT *slot_list = NULL;
 	unsigned int slot_count = 0;
 
-	ctx_log(ctx, 1, "PKCS#11: Initializing the engine\n");
-
-	pkcs11_ctx = PKCS11_CTX_new();
-	PKCS11_CTX_init_args(pkcs11_ctx, ctx->init_args);
-	PKCS11_set_ui_method(pkcs11_ctx, ctx->ui_method, ctx->callback_data);
 
 	/* PKCS11_CTX_load() uses C_GetSlotList() via p11-kit */
 	if (PKCS11_CTX_load(pkcs11_ctx, ctx->module) < 0) {
 		ctx_log(ctx, 0, "Unable to load module %s\n", ctx->module);
 		PKCS11_CTX_free(pkcs11_ctx);
-		return;
+		return 0;
 	}
 
 	/* PKCS11_enumerate_slots() uses C_GetSlotList() via libp11 */
@@ -289,16 +282,37 @@ static void ctx_init_libp11_unlocked(ENGINE_CTX *ctx)
 		ctx_log(ctx, 0, "Failed to enumerate slots\n");
 		PKCS11_CTX_unload(pkcs11_ctx);
 		PKCS11_CTX_free(pkcs11_ctx);
-		return;
+		return 0;
 	}
 
 	ctx_log(ctx, 1, "Found %u slot%s\n", slot_count,
 		slot_count <= 1 ? "" : "s");
 
-	ctx->pkcs11_ctx = pkcs11_ctx;
 	ctx->slot_list = slot_list;
 	ctx->slot_count = slot_count;
+
+	return 1;
 }
+
+
+/* Initialize libp11 data: ctx->pkcs11_ctx and ctx->slot_list */
+static void ctx_init_libp11_unlocked(ENGINE_CTX *ctx)
+{
+	PKCS11_CTX *pkcs11_ctx;
+
+	ctx_log(ctx, 1, "PKCS#11: Initializing the engine\n");
+
+	pkcs11_ctx = PKCS11_CTX_new();
+	PKCS11_CTX_init_args(pkcs11_ctx, ctx->init_args);
+	PKCS11_set_ui_method(pkcs11_ctx, ctx->ui_method, ctx->callback_data);
+
+	if (ctx_enumerate_slots(ctx, pkcs11_ctx) != 1)
+		return;
+
+	ctx->pkcs11_ctx = pkcs11_ctx;
+}
+
+
 
 static int ctx_init_libp11(ENGINE_CTX *ctx)
 {
@@ -1092,6 +1106,8 @@ int ctx_engine_ctrl(ENGINE_CTX *ctx, int cmd, long i, void *p, void (*f)())
 		return ctx_ctrl_set_callback_data(ctx, p);
 	case CMD_FORCE_LOGIN:
 		return ctx_ctrl_force_login(ctx);
+	case CMD_RE_ENUMERATE:
+		return ctx_enumerate_slots(ctx, ctx->pkcs11_ctx);
 	default:
 		ENGerr(ENG_F_CTX_ENGINE_CTRL, ENG_R_UNKNOWN_COMMAND);
 		break;

src/eng_front.c
@@ -75,6 +75,10 @@ static const ENGINE_CMD_DEFN engine_cmd_defns[] = {
 		"FORCE_LOGIN",
 		"Force login to the PKCS#11 module",
 		ENGINE_CMD_FLAG_NO_INPUT},
+	{CMD_RE_ENUMERATE,
+		"RE_ENUMERATE",
+		"re enumerate slots",
+		ENGINE_CMD_FLAG_NO_INPUT},
 	{0, NULL, NULL, 0}
 };
 

src/engine.h
@@ -51,6 +51,7 @@
 #define CMD_SET_USER_INTERFACE	(ENGINE_CMD_BASE + 7)
 #define CMD_SET_CALLBACK_DATA	(ENGINE_CMD_BASE + 8)
 #define CMD_FORCE_LOGIN	(ENGINE_CMD_BASE+9)
+#define CMD_RE_ENUMERATE	(ENGINE_CMD_BASE+10)
 
 typedef struct st_engine_ctx ENGINE_CTX; /* opaque */
 