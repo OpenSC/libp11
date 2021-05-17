56a941a06f0f17f23ed581286d1622fcadff497b
Jakub Jelen <jjelen@redhat.com>
2021-05-17 18:04:10 +0200

    Remove unused openssl 1.x compat macro

--- tests/fork-change-slot.c
+++ tests/fork-change-slot.c
@@ -65,10 +65,6 @@
 #define RANDOM_SIZE 20
 #define MAX_SIGSIZE 1024
 
-#if OPENSSL_VERSION_NUMBER < 0x10100003L
-#define EVP_PKEY_get0_RSA(key) ((key)->pkey.rsa)
-#endif
-
 static int do_wait(pid_t pids[], int num)
 {
     int i;
